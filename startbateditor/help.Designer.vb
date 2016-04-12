2993cf6b9c468f47ba3af518c7a2fba8e882e4b4
Jonathan Barrett <jblackops99@gmail.com>
2016-04-11 21:25:08 -0600

    Content

--- startbateditor/help.Designer.vb
+++ startbateditor/help.Designer.vb
@@ -24,7 +24,14 @@ Partial Class help
     Private Sub InitializeComponent()
         Me.HuraForm1 = New startbateditor.HuraForm()
         Me.HuraGroupBox1 = New startbateditor.HuraGroupBox()
+        Me.HuraButton1 = New startbateditor.HuraButton()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.HuraButton2 = New startbateditor.HuraButton()
         Me.HuraForm1.SuspendLayout()
+        Me.HuraGroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'HuraForm1
@@ -32,30 +39,104 @@ Partial Class help
         Me.HuraForm1.AccentColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.HuraForm1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
         Me.HuraForm1.ColorScheme = startbateditor.HuraForm.ColorSchemes.Dark
+        Me.HuraForm1.Controls.Add(Me.HuraButton1)
         Me.HuraForm1.Controls.Add(Me.HuraGroupBox1)
         Me.HuraForm1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.HuraForm1.Font = New System.Drawing.Font("Segoe UI", 9.5!)
         Me.HuraForm1.ForeColor = System.Drawing.Color.Gray
         Me.HuraForm1.Location = New System.Drawing.Point(0, 0)
         Me.HuraForm1.Name = "HuraForm1"
-        Me.HuraForm1.Size = New System.Drawing.Size(327, 320)
+        Me.HuraForm1.Size = New System.Drawing.Size(307, 302)
         Me.HuraForm1.TabIndex = 0
         Me.HuraForm1.Text = "Help"
         '
         'HuraGroupBox1
         '
         Me.HuraGroupBox1.BackColor = System.Drawing.Color.White
-        Me.HuraGroupBox1.Location = New System.Drawing.Point(15, 30)
+        Me.HuraGroupBox1.Controls.Add(Me.HuraButton2)
+        Me.HuraGroupBox1.Controls.Add(Me.Label2)
+        Me.HuraGroupBox1.Controls.Add(Me.Label1)
+        Me.HuraGroupBox1.Controls.Add(Me.ListBox2)
+        Me.HuraGroupBox1.Controls.Add(Me.ListBox1)
+        Me.HuraGroupBox1.Location = New System.Drawing.Point(12, 33)
         Me.HuraGroupBox1.Name = "HuraGroupBox1"
-        Me.HuraGroupBox1.Size = New System.Drawing.Size(298, 275)
+        Me.HuraGroupBox1.Size = New System.Drawing.Size(282, 258)
         Me.HuraGroupBox1.TabIndex = 0
         Me.HuraGroupBox1.Text = "HuraGroupBox1"
+        '
+        'HuraButton1
+        '
+        Me.HuraButton1.BackColor = System.Drawing.Color.Transparent
+        Me.HuraButton1.BaseColour = System.Drawing.Color.White
+        Me.HuraButton1.BorderColour = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.HuraButton1.FontColour = System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer))
+        Me.HuraButton1.HoverColour = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraButton1.Location = New System.Drawing.Point(272, 3)
+        Me.HuraButton1.Name = "HuraButton1"
+        Me.HuraButton1.PressedColour = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer))
+        Me.HuraButton1.ProgressColour = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraButton1.Size = New System.Drawing.Size(22, 21)
+        Me.HuraButton1.TabIndex = 1
+        Me.HuraButton1.Text = "X"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 17
+        Me.ListBox1.Items.AddRange(New Object() {"spigot-1.9.2.jar", "spigot-1.9.jar", "spigot-1.8.8.jar", "spigot-1.8.3.jar", "spigot-1.8.jar", "spigot1649.jar"})
+        Me.ListBox1.Location = New System.Drawing.Point(3, 26)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(134, 191)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 17
+        Me.ListBox2.Items.AddRange(New Object() {"craftbukkit-1.9.2.jar", "craftbukkit-1.9.jar", "craftbukkit-1.8.8.jar", "craftbukkit-1.8.3.jar", "craftbukkit-1.8.jar"})
+        Me.ListBox2.Location = New System.Drawing.Point(143, 26)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(134, 191)
+        Me.ListBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 6)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Versions of Spigot"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(140, 6)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(138, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Versions of CraftBukkit"
+        '
+        'HuraButton2
+        '
+        Me.HuraButton2.BackColor = System.Drawing.Color.Transparent
+        Me.HuraButton2.BaseColour = System.Drawing.Color.White
+        Me.HuraButton2.BorderColour = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.HuraButton2.FontColour = System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer))
+        Me.HuraButton2.HoverColour = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraButton2.Location = New System.Drawing.Point(3, 223)
+        Me.HuraButton2.Name = "HuraButton2"
+        Me.HuraButton2.PressedColour = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer))
+        Me.HuraButton2.ProgressColour = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(191, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraButton2.Size = New System.Drawing.Size(276, 32)
+        Me.HuraButton2.TabIndex = 4
+        Me.HuraButton2.Text = "Get Spigot/CraftBikkit"
         '
         'help
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(327, 320)
+        Me.ClientSize = New System.Drawing.Size(307, 302)
         Me.ControlBox = False
         Me.Controls.Add(Me.HuraForm1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -65,10 +146,18 @@ Partial Class help
         Me.ShowIcon = False
         Me.Text = "Help"
         Me.HuraForm1.ResumeLayout(False)
+        Me.HuraGroupBox1.ResumeLayout(False)
+        Me.HuraGroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents HuraForm1 As HuraForm
     Friend WithEvents HuraGroupBox1 As HuraGroupBox
+    Friend WithEvents HuraButton1 As HuraButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents HuraButton2 As HuraButton
 End Class
--- startbateditor/help.vb
+++ startbateditor/help.vb
@@ -1,3 +1,9 @@
 ﻿Public Class help
+    Private Sub HuraButton1_Click(sender As Object, e As EventArgs) Handles HuraButton1.Click
+        Me.Close()
+    End Sub
 
+    Private Sub HuraButton2_Click(sender As Object, e As EventArgs) Handles HuraButton2.Click
+        Process.Start("http://getspigot.org/")
+    End Sub
 End Class
